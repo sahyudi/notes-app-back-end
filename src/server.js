5d8b79a6f98e1512c33ce8ba96622f24aa18f167
sahyudi <sahyudi.dev@gmail.com>
2021-06-11 17:22:26 +0700

    tester

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
- 
+const NotesValidator = require('./validator/notes');
+
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
@@ -13,16 +14,17 @@ const init = async () => {
       },
     },
   });
- 
+
   await server.register({
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator
     },
   });
- 
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
